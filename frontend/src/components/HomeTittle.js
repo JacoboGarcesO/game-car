159165229eef30f204ff85c4d2ea35b83fb5a65f
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-03-05 05:18:39 -0500

    Se dejó el frontend en el estado deseado antes de comenzar y se conectó el backend con la BD

---
+++ frontend/src/components/HomeTittle.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const HomeTittle = () => {
+    return (<h1>Home</h1>);
+}
+export default HomeTittle;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeTittle from '../components/HomeTittle';
+
+const Home = () => {
+    return (
+        <>
+            <HomeTittle />
+        </>
+    );
+}
+export default Home;
